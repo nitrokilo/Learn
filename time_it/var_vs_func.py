722dba7e25d35fc252ecf4c3fcad8cac33d3ae06
Jake Itegboje <jakeitegsy@yahoo.com>
2021-09-20 01:59:21 -0500

    add tests for other primitives

--- time_it/var_vs_func.py
+++ time_it/var_vs_func.py
@@ -1,19 +1,82 @@
+from timeit import timeit
 
-import timeit
+def variable_tester(value):
+    return timeit(f'''
+def tester():
+    variable = {value}
+    return variable
+    ''')
 
+def function_tester(value):
+    return timeit(f'''
+def function():
+    return {value}
+def tester():
+    return function()
+    ''')
 
-#insert ife into "testcode" to test variable speed
-ife = "ife"
+def test_boolean_variable():
+    return variable_tester(True)
 
+def test_function_returns_boolean():
+    return function_tester(True)
 
-testcode= """
-def f_ife():
-    return "ife"
-"""
+def test_int_variable():
+    return variable_tester(1)
 
+def test_function_returns_int():
+    return function_tester(1)
 
+def test_float_variable():
+    return variable_tester(1.0)
 
+def test_function_returns_float():
+    return function_tester(1.0)
 
-result = timeit.timeit(stmt= testcode)
+def test_string_variable():
+    return variable_tester('string')
 
-print(result)
+def test_function_returns_string():
+    return function_tester('string')
+
+def test_tuple_variable():
+    return variable_tester((1, 2, 3))
+
+def test_function_returns_tuple():
+    return function_tester((1, 2 , 3))
+
+def test_list_variable():
+    return variable_tester([1, 2, 3])
+
+def test_function_returns_list():
+    return function_tester([1, 2, 3])
+
+def test_set_variable():
+    return variable_tester({1, 2, 3})
+
+def test_function_returns_set():
+    return function_tester({1, 2, 3})
+
+def test_dictionary_variable():
+    return variable_tester({'a': 1, 'b': 2, 'c': 3})
+
+def test_function_returns_dictionary():
+    return function_tester({'a': 1, 'b': 2, 'c': 3})
+
+print(f'Calling Function that returns boolean runs in : {test_function_returns_boolean()} seconds')
+print(f'Calling boolean Variable runs in : {test_boolean_variable()} seconds')
+print(f'Calling Function that returns int runs in : {test_function_returns_int()} seconds')
+print(f'Calling int Variable runs in : {test_int_variable()} seconds')
+print(f'Calling Function that returns float runs in : {test_function_returns_float()} seconds')
+print(f'Calling float Variable runs in : {test_float_variable()} seconds')
+print(f'Calling Function that returns string runs in : {test_function_returns_string()} seconds')
+print(f'Calling string Variable runs in : {test_string_variable()} seconds')
+print(f'Calling Function that returns tuple runs in : {test_function_returns_tuple()} seconds')
+print(f'Calling tuple Variable runs in : {test_tuple_variable()} seconds')
+print(f'Calling Function that returns list runs in : {test_function_returns_list()} seconds')
+print(f'Calling list Variable runs in : {test_list_variable()} seconds')
+print(f'Calling Function that returns set runs in : {test_function_returns_set()} seconds')
+print(f'Calling set Variable runs in : {test_set_variable()} seconds')
+print(
+    f'Calling Function that returns dictionary runs in : {test_function_returns_dictionary()} seconds')
+print(f'Calling dictionary Variable runs in : {test_dictionary_variable()} seconds')
